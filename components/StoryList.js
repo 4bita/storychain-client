8ebdb984ebf6fc842c0dfed0af419170035d111e
Dima Krutolianov <DimaKrutDK@ya.ru>
2022-08-10 08:08:36 +0200

    Change header UI

--- components/StoryList.js
+++ components/StoryList.js
@@ -1,8 +1,7 @@
 import { useRouter } from 'next/router';
-import { useState, useEffect, useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import Story from "./Story";
-import MyInput from "./UI/input/MyInput";
 import SpinnerCat from "./UI/spinner/SpinnerCat";
 import { ServerContext } from "./AppContext";
 import { loadStories } from '../lib/loader';
@@ -14,7 +13,7 @@ const StoryList = () => {
     const context = useContext(ServerContext);
 
     useEffect(() => {
-        if(context){
+        if (context) {
             loadStories(context, setStories).catch(console.error);
         }
     }, [context]);
@@ -25,14 +24,7 @@ const StoryList = () => {
 
     return (
         <div>
-            <h3 style={{textAlign: 'center', marginTop: '40px'}}>List of stories...</h3>
-
-            <MyInput
-                type='text'
-                placeholder='Find a story'
-                style={{marginTop: '20px'}}
-            />
-            <hr />
+            <h3 style={{ textAlign: 'center', marginTop: '40px' }}>List of stories...</h3>
 
             {
                 stories === null
