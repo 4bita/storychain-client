9b8693ceaff00b97ed402ef6438a916aed3245f1
Dima Krutolianov <DimaKrutDK@ya.ru>
2022-08-15 17:29:50 +0200

    Fix dom nesting warning

--- components/Story.js
+++ components/Story.js
@@ -7,9 +7,9 @@ const Story = ({ title, id, content }) => {
         <Link href={`/stories/${id}`}>
             <a className="block rounded-sm mb-3 text-gray-600 hover:text-gray-600 active:text-gray-600 visited:text-gray-600 hover:border-gray-500 !border border-gray-300 bg-white p-4">
                 <h6 className="font-medium text-gray-700 mb-2">{title}</h6>
-                <p className="text-gray-500">
+                <div className="text-gray-500">
                     <ReactMarkdown>{content}</ReactMarkdown>
-                </p>
+                </div>
             </a>
         </Link>
     );
