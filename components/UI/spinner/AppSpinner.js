36253e6e11a5e9fa364b6f80baef6eacf2f9fcdd
Dima Krutolianov <DimaKrutDK@ya.ru>
2022-08-16 08:35:10 +0200

    Change spinner to common circle, center it

---
+++ components/UI/spinner/AppSpinner.js
@@ -0,0 +1,11 @@
+import { Spinner } from "reactstrap";
+
+const AppSpinner = () => {
+    return (
+        <Spinner color="primary">
+            Loading...
+        </Spinner>
+    );
+};
+
+export default AppSpinner;
